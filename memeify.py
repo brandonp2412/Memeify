e43cb93622a370b3717f613146fc285fbc5d0b52
Brandon Presley <brandonp2412@gmail.com>
2018-07-10 00:19:55 +1200

    Renamed command

--- memeify.py
+++ memeify.py
@@ -10,7 +10,7 @@ client = discord.Client()
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('!memify'):
+    if message.content.startswith('!memeify'):
         params = message.content.split()
         memes = [random.choice(RANDOM_EMOJIS) for param in params[1:]]
         meme_text = []
@@ -18,10 +18,15 @@ async def on_message(message):
             meme_text.append(meme)
             meme_text.append(param)
         meme_text = ' '.join(meme_text)
+        log(message, meme_text)
         await client.send_message(message.channel, meme_text)
 
 @client.event
 async def on_ready():
-    print(f'Logged in as: {client.user.name}')
+    print(f'[INFO] -- Logged in as: {client.user.name}')
+
+def log(message, meme_text):
+    print(f"[INFO] -- User: {message.author} inputted: {message.content}")
+    print(f"[INFO] -- User: {message.author} got output: {meme_text}")
 
 client.run(TOKEN)
